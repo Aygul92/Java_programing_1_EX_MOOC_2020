aa2637193b41c11fb2243b5bb5c7255ed42ce2f0
Aygul92 <ayoo_66@hotmail.com>
2020-10-21 11:26:05 +0200

    Add files via upload

---
+++ Part01_33.Password/74df4823-6acc-4c66-bba7-49eff3c1ed0f-part01-Part01_33.Password-8602631/src/main/main/java/Password.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+public class Password {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        // Write your program here 
+        System.out.println("Password?");
+        String pw = scan.nextLine();
+
+        System.out.println(!pw.equals("Caput Draconis") ? "Off with you!" : "Welcome!");
+    }
+}
